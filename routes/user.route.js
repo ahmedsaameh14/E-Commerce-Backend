2067e9e37fa9b7ddba9d2c1f72cb951c6028ef33
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-07-01 19:39:48 +0300

    Authorize at User - Admin

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {createUser , getUser} = require('../controllers/user.controller');
 const {authenticate}= require('../middlewares/auth.middleware')
+const { authorize } = require('../middlewares/role.middleware')
 
 
-router.get('/', getUser);
+router.get('/', authenticate,authorize('admin'),getUser);
+router.post('/createAdmin',authenticate,authorize('admin'),createUser('admin'));        // Only Admin can Create New Admins
 router.post('/',createUser('user'));
 
 module.exports = router;
