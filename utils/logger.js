e92656564acf7bf8a45b944b26e0ca06a6081b6c
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-06-14 09:50:23 +0300

    Fix Logging - Winston

--- utils/logger.js
+++ utils/logger.js
@@ -1,33 +1,30 @@
+const winston = require('winston');
 const fs = require('fs');
 const path = require('path');
-const winston = require('winston');
 
-const logDirectory = path.join(__dirname, '../logs');
-if (!fs.existsSync(logDirectory)) {
-  fs.mkdirSync(logDirectory, { recursive: true });
-}
+const transports = [];
 
-const logger = winston.createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.errors({ stack: true }),
-    winston.format.splat(),
-    winston.format.json()
-  ),
-  transports: [
+if (process.env.NODE_ENV === 'production') {
+  transports.push(
+    new winston.transports.Console()
+  );
+} else {
+  const logDirectory = path.join(__dirname, '../logs');
+
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory, { recursive: true });
+  }
+
+  transports.push(
     new winston.transports.File({
       filename: path.join(logDirectory, 'error.log'),
       level: 'error'
     }),
+
     new winston.transports.File({
       filename: path.join(logDirectory, 'combined.log')
-    })
-  ]
-});
+    }),
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize(),
@@ -37,10 +34,23 @@ if (process.env.NODE_ENV !== 'production') {
   );
 }
 
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'production'
+    ? 'info'
+    : 'debug',
+
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+
+  transports
+});
+
 logger.stream = {
-  write: message => {
-    logger.info(message.trim());
-  }
+  write: message => logger.info(message.trim())
 };
 
 module.exports = logger;
